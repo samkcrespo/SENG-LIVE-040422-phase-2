39e7369680716b6fd4f7a55962dab06489aac06d
Aysan Isayo <aisayo@icloud.com>
2022-04-29 12:11:15 -0400

    lecture 5 starter code

---
+++ 05-react-forms/README.md
@@ -0,0 +1,14 @@
+# React Forms
+
+## Objectives
+
+- Explain why we use controlled forms (vs uncontrolled forms)
+- Implement a controlled form
+- Use form data to update state
+
+## Deliverables
+
+- Create a form and make it controlled
+  - Input change handlers
+  - Form submit handler
+- Handle submitting the form and update state in parent using inverse data flow
---
+++ 05-react-forms/project_showcase/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const fetchProjects = () => {
+    fetch("http://localhost:4000/projects")
+      .then((resp) => resp.json())
+      .then((projects) => setProjects(projects));
+  };
+
+  const onToggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
+      <ProjectForm />
+      <button onClick={fetchProjects}>Load Projects</button>
+      <ProjectList projects={projects} />
+    </div>
+  );
+};
+
+export default App;
