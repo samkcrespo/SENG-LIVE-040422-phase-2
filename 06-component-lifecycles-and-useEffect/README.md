758ec58b325fd922e6297d32bcf3b05dacb74bf9
Aysan Isayo <aisayo@icloud.com>
2022-05-02 09:08:13 -0400

    lecture 6 starter code

---
+++ 06-component-lifecycles-and-useEffect/README.md
@@ -0,0 +1,11 @@
+# React Component Lifecycles and useEffect
+
+## Objectives
+
+- Describe the three phases of a react component life cycles
+- Understand when side effects can occur 
+- Implement the useEffect hook to fetch data
+
+## Deliverables
+
+- Implement useEffect in App component to load projects
---
+++ 06-component-lifecycles-and-useEffect/project_showcase/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const fetchProjects = () => {
+    fetch("http://localhost:4000/projects")
+      .then((resp) => resp.json())
+      .then((projects) => setProjects(projects));
+  };
+
+  const onToggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
+      <ProjectForm />
+      <button onClick={fetchProjects}>Load Projects</button>
+      {/* <button onClick={() => setText("hello world")}>Hello World</button>
+      <button onClick={() => setText("goodbye")}>Goodbye</button> */}
+      <ProjectList projects={projects} />
+    </div>
+  );
+};
+
+export default App;
