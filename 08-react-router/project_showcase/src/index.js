6c89fd7d4454907a04be8bb119181d34ac4a0e35
Aysan Isayo <aisayo@icloud.com>
2022-05-06 10:06:19 -0400

    fixed index.js

--- 08-react-router/project_showcase/src/index.js
+++ 08-react-router/project_showcase/src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
